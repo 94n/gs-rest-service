39babd55bcefd3810e0a97ee5e93bdf167442ce3
makstitoff <makstitoff@gmail.com>
2020-01-17 18:04:30 +0200

    tests

--- complete/src/test/java/route/api/ApplicationTests.java
+++ complete/src/test/java/route/api/ApplicationTests.java
@@ -22,13 +22,20 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import route.task.PaymentTask;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
+import static org.springframework.test.util.AssertionErrors.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -38,15 +45,44 @@ public class ApplicationTests {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private PaymentTask paymentTask;
+
     @Test
-    public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
-        short route = new Random().nextInt()
-        this.mockMvc.perform(get("/create?route=120&time=2020-01-15-10-10")).andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$").isNumber());
+    public void checkNewUnprocessedTripShouldReturnEmptyId() throws Exception {
+        short route = (short) new Random().nextInt(Short.MAX_VALUE);
+        long randomTime = ThreadLocalRandom.current().nextLong(Instant.now().getEpochSecond(), LocalDateTime.of(2099, 12, 31, 23, 59).toInstant(ZoneId.systemDefault().getRules().getOffset(LocalDateTime.now())).getEpochSecond());
+        String routeTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(randomTime), ZoneId.systemDefault()).format(DateTimeFormatter.ofPattern("yyyy-MM-dd-hh-mm"));
+        String tripId = this.mockMvc.perform(get("/create?route=" + route + "&time=" + routeTime)).andReturn().getResponse().getContentAsString();
+        this.mockMvc.perform(get("/check?id=" + tripId)).andExpect(status().isOk()).andExpect(content().string(""));
     }
 
     @Test
-    public void paramGreetingShouldReturnTailoredMessage() throws Exception {
-        this.mockMvc.perform(get("/greeting").param("name", "Spring Community")).andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
+    public void createRoutePaymentShouldReturnNumber() throws Exception {
+        short route = (short) new Random().nextInt(Short.MAX_VALUE);
+        long randomTime = ThreadLocalRandom.current().nextLong(Instant.now().getEpochSecond(), LocalDateTime.of(2099, 12, 31, 23, 59).toInstant(ZoneId.systemDefault().getRules().getOffset(LocalDateTime.now())).getEpochSecond());
+        String routeTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(randomTime), ZoneId.systemDefault()).format(DateTimeFormatter.ofPattern("yyyy-MM-dd-hh-mm"));
+        this.mockMvc.perform(get("/create?route=" + route + "&time=" + routeTime)).andExpect(status().isOk()).andExpect(jsonPath("$").isNumber());
+    }
+
+    @Test
+    public void payShouldReturnKnownState() throws Exception {
+        List<String> states = Arrays.asList("processing", "error", "done");
+        assertTrue("incorrect state after payment start", states.contains(this.mockMvc.perform(get("/pay")).andReturn().getResponse().getContentAsString()));
+    }
+
+    @Test
+    public void paymentTaskShouldSetKnownState() throws Exception {
+        List<String> states = Arrays.asList("processing", "error", "done");
+        short route = (short) new Random().nextInt(Short.MAX_VALUE);
+        long randomTime = ThreadLocalRandom.current().nextLong(Instant.now().getEpochSecond(), LocalDateTime.of(2099, 12, 31, 23, 59).toInstant(ZoneId.systemDefault().getRules().getOffset(LocalDateTime.now())).getEpochSecond());
+        String routeTime = LocalDateTime.ofInstant(Instant.ofEpochSecond(randomTime), ZoneId.systemDefault()).format(DateTimeFormatter.ofPattern("yyyy-MM-dd-hh-mm"));
+        this.mockMvc.perform(get("/create?route=" + route + "&time=" + routeTime)).andReturn().getResponse().getContentAsString();
+        try {
+            paymentTask.tryPayment();
+        } catch (Exception e) {
+            assertTrue("error during task", false);
+        }
     }
 
 }
